7688b4d0f8784fbe8974b6a2601685699366a0cf
Rajeshwari <arajeshwari2022@gmail.com>
2022-09-08 23:17:48 +0530

    case and ielseif files added

--- 11-ifelse.sh
+++ 11-ifelse.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 a= $1
 
-if ["$a" = "true"]
+if ["$a" == "true"]
     then
-    echo "true condition is executed"
+    echo true condition is executed
 
-    elif ["$a" = "false"]
+    elif ["$a" =="false"]
     then
-    echo "false statement is executed"
+    echo false statement is executed
 
     else
     echo "no statement is executed"
-
     fi;
 
 
