d47be6d698bdc63c0180d357224d1521debed3ae
Rajeshwari <arajeshwari2022@gmail.com>
2022-09-08 22:47:43 +0530

    syntax of case added

--- 09-conditions.sh
+++ 09-conditions.sh
@@ -1,14 +1,25 @@
+#!/bin/bash 
 
+#syntax
+# case $var  in 
+#     cond1)
+#         command1 ;; 
+#     cond2)
+#         command2 ;;
+#     *)
+#         exz ;;;
+# esac
 
-read action
+action = $1
+#read action // read or dollar any thing can be selected.
 case "$action" in
     start)
-        echo "start the srvive"
+        echo "start the service"
         ;;
     end)
         echo end the servie
         ;;
     *)
-        echo "option not selected"
+        echo "\e[32m Valid options are start or stop or restart only \e[0m"
         ;;
     esac
