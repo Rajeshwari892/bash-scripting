1bad660eac03011474ab81d8e7af93613e334c3f
Rajeshwari <arajeshwari2022@gmail.com>
2022-09-08 23:22:34 +0530

    case and ielseif files added

--- 11-ifelse.sh
+++ 11-ifelse.sh
@@ -4,7 +4,7 @@ a= $1
 if [ "$a" == "true" ]; then
     echo true condition is executed
 
-    elif [ "$a" =="false" ];  then
+    elif [ "$a" == "false" ];  then
     echo false statement is executed
 
     else
