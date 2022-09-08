143da7815bb3b9ec31c3053e8f299b02e735219c
Rajeshwari <arajeshwari2022@gmail.com>
2022-09-08 23:22:00 +0530

    case and ielseif files added

--- 11-ifelse.sh
+++ 11-ifelse.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 a= $1
 
-if ["$a" == "true"]; then
+if [ "$a" == "true" ]; then
     echo true condition is executed
 
-    elif ["$a" =="false"];  then
+    elif [ "$a" =="false" ];  then
     echo false statement is executed
 
     else
