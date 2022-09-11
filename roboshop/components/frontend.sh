2a3656d9a25f1525c74cd179f2735dce67be9cbd
Rajeshwari <arajeshwari2022@gmail.com>
2022-09-11 18:58:24 +0530

    adding script to frontend.sh

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -4,12 +4,32 @@ set -e # to stop execution of further steps at the point of failure
 source components/common.sh
 
 #install nginx
-echo  -n "install nginx" # -n is used to get the commands in next line
+echo -n "install nginx: " # -n is used to get the commands in next line
 
 yum install nginx -y  >> /tmp/frontend.log
 
 systemctl enable nginx
 
-echo -n "starting nginx"
+echo -n "starting nginx: "
 systemctl start nginx
 stat $?
+
+echo -n "Downloading the Code"
+curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend/archive/main.zip"
+stat $?
+
+echo -n "Downloading the Code: "
+ cd /usr/share/nginx/html
+ rm -rf *
+ unzip /tmp/frontend.zip >> /tmp/frontend.logs
+ mv frontend-main/* .
+ mv static/* .
+ rm -rf frontend-main README.md
+ mv localhost.conf /etc/nginx/default.d/roboshop.conf
+
+echo -n "restarting the service: "
+
+ systemctl daemon-reload
+ systemctl restart nginx
+
+
