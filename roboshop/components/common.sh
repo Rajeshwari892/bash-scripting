0cdcb49ea97c3e682de154b587d87b6a0f39f89c
Rajeshwari <arajeshwari2022@gmail.com>
2022-09-11 18:23:27 +0530

    adding frontend.sh

---
+++ roboshop/components/common.sh
@@ -0,0 +1,19 @@
+# validating whether the executing user is root or not
+ID=$(id -u)
+if [ ID -ne 0 ]; then
+   echo -e "\e[32m Try executing as sudo or a root user \e[0m"
+   exit 1
+fi;
+
+#Declaring the function to check the executed commands success state
+# if status of cmd is 1 then it is executed properly. $
+
+stat() {
+    if [ $1 -eq 0 ]; then
+        echo -e "\e[32m succes \e[0m"
+    else
+        echo -e "\e[32m Failure. check for the logs. \e[0m"
+    fi
+}
+
+
--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -1,1 +1,10 @@
-echo stated frontend
+
+set -e # to stop execution of further steps at the point of failure 
+
+source components/common.sh
+
+#install nginx
+
+systemctl enable nginx
+systemctl enable nginx
+systemctl start nginx
