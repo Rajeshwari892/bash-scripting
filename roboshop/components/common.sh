8d1f1acbd64de2144ae17715e27d16cc5fd0307e
Rajeshwari <arajeshwari2022@gmail.com>
2022-09-11 18:26:45 +0530

    adding frontend.sh

--- roboshop/components/common.sh
+++ roboshop/components/common.sh
@@ -1,6 +1,6 @@
 # validating whether the executing user is root or not
 ID=$(id -u)
-if [ ID -ne 0 ]; then
+if [ $ID -ne 0 ]; then
    echo -e "\e[32m Try executing as sudo or a root user \e[0m"
    exit 1
 fi;
