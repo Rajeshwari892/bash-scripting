71256f98c1ed4e2bcc5b7e4d9033140c3d657cc6
Rajeshwari <arajeshwari2022@gmail.com>
2022-09-12 00:50:01 +0530

    catalogu file e aadded

--- roboshop/components/catalogue.sh
+++ roboshop/components/catalogue.sh
@@ -16,8 +16,9 @@ echo -n "adding roboshop user: "
 id roboshop >> /tmp/catalogue.log || useradd roboshop # Creates users only in case if the user account doen's exist
 stat $?
 
-echo -n "Downloading the  catalogue"
+echo -n "Downloading the  catalogue: "
 curl -s -L -o /tmp/catalogue.zip "https://github.com/stans-robot-project/catalogue/archive/main.zip" >>  /tmp/catalogue.log
+stat $?
 
 echo -n "cleanup of old catalogue content: "
 rm -rf /home/roboshop/catalogue >> /tmp/catalogue.log
@@ -34,6 +35,17 @@ stat $?
 
 echo -n "Installing catalogue Dependencies: "
 cd catalogue && npm install &>> /tmp/catalogue.log
+stat $?
+
+echo -n "Configuring the systemd file"
+sed -i -e 's/MONGO_DNSNAME/mongodb-roboshop-internal/' /home/roboshop/catalogue/systemd.service
+mv /home/roboshop/catalogue/systemd.service /etc/systemd/system/catalogue.service
+stat $? 
+
+echo -n "Starting the service"
+systemctl daemon-reload  &>> /tmp/catalogue.log 
+systemctl enable catalogue &>> /tmp/catalogue.log
+systemctl start ${COMPONENT} &>> /tmp/cataloguelog
 stat $?
 
 
