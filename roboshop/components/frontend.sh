4a9bd9404686f1fe36d0eb7db8bb7c4a20c0b4d4
Rajeshwari <arajeshwari2022@gmail.com>
2022-09-11 18:40:10 +0530

    adding script to frontend.sh

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -4,8 +4,12 @@ set -e # to stop execution of further steps at the point of failure
 source components/common.sh
 
 #install nginx
-echo  -n "start download nginx" # -n is used to get the commands in next line
+echo  -n "install nginx" # -n is used to get the commands in next line
 
-#systemctl enable nginx
-#systemctl enable nginx
-#systemctl start nginx
+yum install nginx -y 
+
+systemctl enable nginx
+
+echo -n "starting nginx"
+systemctl start nginx
+stat $?
