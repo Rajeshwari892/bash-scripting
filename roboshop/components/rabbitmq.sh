6eee0d892ebc8daf07983392b07c8bebd7489af3
Rajeshwari <arajeshwari2022@gmail.com>
2022-09-29 20:14:01 +0530

    small changes

--- roboshop/components/rabbitmq.sh
+++ roboshop/components/rabbitmq.sh
@@ -7,7 +7,7 @@ source components/common.sh
 COMPONENT=rabbitmq
 
 echo -n "Erlang dependency configuring for the $COMPONENT repo: "
-yum install https://github.com/rabbitmq/erlang-rpm/releases/download/v23.2.6/erlang-23.2.6-1.el7.x86_64.rpm -y  &>> ${LOGFILE} 
+yum install https://github.com/rabbitmq/erlang-rpm/releases/download/v23.2.6/erlang-23.2.6-1.el7.x86_64.rpm -y  &>> ${LOGFILE}
 stat $?
 
 echo -n "configuring and installing the $COMPONENT repo"
