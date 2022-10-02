eba3007ae37795452d63caf6ec00e0a5338ce027
Rajeshwari <arajeshwari2022@gmail.com>
2022-10-03 01:18:33 +0530

    Changes in launch-ec2.sh

--- roboshop/launch-ec2.sh
+++ roboshop/launch-ec2.sh
@@ -9,9 +9,18 @@ fi
 
 COMPONENT=$1 #after sudo bash launch-ec2 value($1= frontendor user or catalogue), z expects value; empty throws error
 
-AMI_ID=$(aws ec2 describe-images  --filters "Name=name,Values=DevOps-LabImage-CentOS7" |  jq '.Images[].ImageId'| sed -e 's/"//g')
+ZONEID=Z025435221B0U2ES6A056
+AMI_ID=$(aws ec2 describe-images  --filters "Name=name,Values=DevOps-LabImage-CentOS7"  | jq '.Images[].ImageId'| sed -e 's/"//g')
 SGID="sg-0092586f6f714fc1b"  
 echo "ami id id which we are using is $AMI_ID"
 
-aws ec2 run-instances --image-id ${AMI_ID} --instance-type t2.micro  --security-group-ids ${SGID}  --tag-specifications "ResourceType=instance,Tags=[{Key=webserver,Value=production}]"
- 
+PRIVATE_IP=$(aws ec2 run-instances --image-id ${AMI_ID} --instance-type t2.micro  --security-group-ids ${SGID}  --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]"
+--instance-market-options "MarketType=spot, SpotOptions={SpotInstanceType=persistent,InstanceInterruptionBehavior=stop}" | Jq '.Instances[].PrivateIpAddress' | sed -e 's/"//g')
+
+echo " private ip of the created machine is $PRIVATE_IP"
+echo "Spot Instance $COMPONENT is ready: "
+echo "Creating Route53 Record . . . . :"
+
+sed -e "s/COMPONENT/${COMPONENT}" -e "s/PRIVATEIP/${PRIVATE_IP}" r53.json >/tmp/record.json
+
+$ aws route53 change-resource-record-sets --hosted-zone-id ${ZONEID} --change-batch /tmp/record.json
