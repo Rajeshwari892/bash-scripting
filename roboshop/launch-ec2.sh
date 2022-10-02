8fb90ae920157f2c0f698d7d72e4c63a9cc1476c
Rajeshwari <arajeshwari2022@gmail.com>
2022-10-03 01:26:02 +0530

    Changes in launch-ec2.s, r53,json added

--- roboshop/launch-ec2.sh
+++ roboshop/launch-ec2.sh
@@ -17,10 +17,10 @@ echo "ami id id which we are using is $AMI_ID"
 PRIVATE_IP=$(aws ec2 run-instances --image-id ${AMI_ID} --instance-type t2.micro  --security-group-ids ${SGID}  --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]"
 --instance-market-options "MarketType=spot, SpotOptions={SpotInstanceType=persistent,InstanceInterruptionBehavior=stop}" | Jq '.Instances[].PrivateIpAddress' | sed -e 's/"//g')
 
-echo " private ip of the created machine is $PRIVATE_IP"
+echo "private ip of the created machine is $PRIVATE_IP"
 echo "Spot Instance $COMPONENT is ready: "
 echo "Creating Route53 Record . . . . :"
 
-sed -e "s/COMPONENT/${COMPONENT}" -e "s/PRIVATEIP/${PRIVATE_IP}" r53.json >/tmp/record.json
+sed -e "s/COMPONENT/${COMPONENT}/" -e "s/PRIVATEIP/${PRIVATE_IP}/" r53.json >/tmp/record.json
 
 $ aws route53 change-resource-record-sets --hosted-zone-id ${ZONEID} --change-batch /tmp/record.json
