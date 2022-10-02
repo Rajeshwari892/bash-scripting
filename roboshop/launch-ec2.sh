1ed79d1529d83f845d54fd123823b1a28162cc52
Rajeshwari <arajeshwari2022@gmail.com>
2022-10-03 01:37:12 +0530

    Changes in launch-ec2.s, r53,json added

--- roboshop/launch-ec2.sh
+++ roboshop/launch-ec2.sh
@@ -9,7 +9,7 @@ fi
 
 COMPONENT=$1 #after sudo bash launch-ec2 value($1= frontendor user or catalogue), z expects value; empty throws error
 
-ZONEID=Z025435221B0U2ES6A056
+ZONEID="Z025435221B0U2ES6A056"
 AMI_ID=$(aws ec2 describe-images  --filters "Name=name,Values=DevOps-LabImage-CentOS7"  | jq '.Images[].ImageId'| sed -e 's/"//g')
 SGID="sg-0092586f6f714fc1b"  
 echo "ami id id which we are using is $AMI_ID"
