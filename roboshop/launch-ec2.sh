b5f8ef9219df89d96f2a90d47f255f931d12db50
Rajeshwari <arajeshwari2022@gmail.com>
2022-10-02 13:59:39 +0530

    ADDED launch-ec2.sh

--- roboshop/launch-ec2.sh
+++ roboshop/launch-ec2.sh
@@ -3,5 +3,5 @@
 #AMI_ID="ami-0d2238d1970144264" 
 
 AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=DevOps-LabImage-CentOS7" | jq '.Images[].ImagesId' | sed -e 's/"//g')
-echo "ami-id id which we are using is: " $AMI_ID
+echo "ami-id id which we are using is: $AMI_ID"
 
