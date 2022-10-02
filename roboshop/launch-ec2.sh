c8a3debd1663a85a6e7115b6723f8e7f58e04d53
Rajeshwari <arajeshwari2022@gmail.com>
2022-10-03 01:54:40 +0530

    Changes in launch-ec2.sh

--- roboshop/launch-ec2.sh
+++ roboshop/launch-ec2.sh
@@ -9,19 +9,32 @@ fi
 
 COMPONENT=$1 #after sudo bash launch-ec2 value($1= frontendor user or catalogue), z expects value; empty throws error
 
-#ZONEID="Z025435221B0U2ES6A056"
+ENV=$2
+ZONEID="Z025435221B0U2ES6A056"
 AMI_ID=$(aws ec2 describe-images  --filters "Name=name,Values=DevOps-LabImage-CentOS7"  | jq '.Images[].ImageId'| sed -e 's/"//g')
 SGID="sg-0092586f6f714fc1b"  
 echo "ami id id which we are using is $AMI_ID"
 
-PRIVATE_IP=$(aws ec2 run-instances --image-id ${AMI_ID} --instance-type t2.micro  --security-group-ids ${SGID}  --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]" --instance-market-options "MarketType=spot, SpotOptions={SpotInstanceType=persistent,InstanceInterruptionBehavior=stop}"| jq '.Instances[].PrivateIpAddress' | sed -e 's/"//g')
-#PRIVATE_IP=$(aws ec2 run-instances --image-id ${AMI_ID} --instance-type t3.micro  --security-group-ids ${SGID}  --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}-${ENV}}]" --instance-market-options "MarketType=spot, SpotOptions={SpotInstanceType=persistent,InstanceInterruptionBehavior=stop}"| jq '.Instances[].PrivateIpAddress' | sed -e 's/"//g')
+create-server(){
+    PRIVATE_IP=$(aws ec2 run-instances --image-id ${AMI_ID} --instance-type t2.micro  --security-group-ids ${SGID}  --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]" --instance-market-options "MarketType=spot, SpotOptions={SpotInstanceType=persistent,InstanceInterruptionBehavior=stop}"| jq '.Instances[].PrivateIpAddress' | sed -e 's/"//g')
 
-echo "private ip of the created machine is $PRIVATE_IP"
-echo "Spot Instance $COMPONENT is ready: "
-echo "Creating Route53 Record . . . . :"
 
-sed -e "s/PRIVATEIP/${PRIVATE_IP}/" -e "s/COMPONENT/${COMPONENT}/" r53.json  >/tmp/record.json 
+    echo "private ip of the created machine is $PRIVATE_IP"
+    echo "Spot Instance $COMPONENT is ready: "
+    echo "Creating Route53 Record . . . . :"
 
-aws route53 change-resource-record-sets --hosted-zone-id Z025435221B0U2ES6A056 --change-batch file:///tmp/record.json | jq 
+    sed -e "s/PRIVATEIP/${PRIVATE_IP}/" -e "s/COMPONENT/${COMPONENT}-${ENV}/" r53.json  >/tmp/record.json 
+
+    aws route53 change-resource-record-sets --hosted-zone-id ${ZONEID}  --change-batch file:///tmp/record.json | jq 
+
+}
+if [ "$1" == "all" ] ; then 
+    for component in catalogue cart shipping mongodb payment rabbitmq redis mysql user frontend; do 
+        COMPONENT=$component
+        # calling function
+        create-server
+     done
+else 
+     create-server
+fi 
 
