357b1fe73073f584e43739b6c80272ac243a5ecb
Rajeshwari <arajeshwari2022@gmail.com>
2022-10-03 01:38:33 +0530

    Changes in launch-ec2.sh

--- roboshop/launch-ec2.sh
+++ roboshop/launch-ec2.sh
@@ -9,7 +9,7 @@ fi
 
 COMPONENT=$1 #after sudo bash launch-ec2 value($1= frontendor user or catalogue), z expects value; empty throws error
 
-ZONEID="Z025435221B0U2ES6A056"
+#ZONEID="Z025435221B0U2ES6A056"
 AMI_ID=$(aws ec2 describe-images  --filters "Name=name,Values=DevOps-LabImage-CentOS7"  | jq '.Images[].ImageId'| sed -e 's/"//g')
 SGID="sg-0092586f6f714fc1b"  
 echo "ami id id which we are using is $AMI_ID"
@@ -23,5 +23,5 @@ echo "Creating Route53 Record . . . . :"
 
 sed -e "s/PRIVATEIP/${PRIVATE_IP}/" -e "s/COMPONENT/${COMPONENT}/" r53.json  >/tmp/record.json 
 
-aws route53 change-resource-record-sets --hosted-zone-id ${ZONEID} --change-batch file:///tmp/record.json | jq 
+aws route53 change-resource-record-sets --hosted-zone-id Z025435221B0U2ES6A056 --change-batch file:///tmp/record.json | jq 
 
