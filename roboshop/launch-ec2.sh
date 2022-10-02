c1952ff0c1f7d6fa6c0a3f3c11fe705af0913ab8
Rajeshwari <arajeshwari2022@gmail.com>
2022-10-02 13:35:10 +0530

    ADDED launch-ec2.sh

---
+++ roboshop/launch-ec2.sh
@@ -0,0 +1,11 @@
+#AMI_ID="ami-0d2238d1970144264" 
+if [ z  "$1" ]; then # $1 expects the value, after sudo bash launch-ec2  value($1= frontendor user or catalogue), z expects value; empty throws error
+    echo -e "\e[32m Machine name  is Missing \e[0m"
+    exit 1
+fi
+
+
+
+AMI_ID=$(aws ec2 describe-images --filter "Name=name,Values=DevOps-LabImage-CentOS7" | jq '.Images[].ImagesId' | sed -e 's/"//g')
+echo -n "ami-id id: " $AMI_ID
+
