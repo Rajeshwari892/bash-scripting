8d25c8b619a61ca610aecab81eab96f5f18b80f3
Rajeshwari <arajeshwari2022@gmail.com>
2022-11-13 13:05:48 +0530

    component-env updated

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -18,7 +18,7 @@ cd /usr/share/nginx/html
 rm -rf *
 
 echo -n "Extract the zip file: "
-unzip -o /tmp/frontend.zip >> /tmp/frontend.log
+unzip -o /tmp/frontend.zip &>> /tmp/frontend.log
 stat $?
 mv frontend-main/* .
 mv static/* .
@@ -33,7 +33,7 @@ stat $?
 echo -n "update the proxy file in Nginx with the CATALOGUE, CART, USER server IP Address in the FRONTEND Server: "
 
 for component in catalogue user cart shipping payment; do
-    sed -i -e "/${component}/s/localhost/${component}-${ENV}.roboshop.internal/"  /etc/nginx/default.d/roboshop.conf
+    sed -i -e "/${component}/s/localhost/${component}.roboshop.internal/"  /etc/nginx/default.d/roboshop.conf
     stat $?
 done
 
